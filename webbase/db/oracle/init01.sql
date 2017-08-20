ed19a7051ac7e085ed74c984dd47cbcd9d358310
wjacl <wang_jian_an@139.com>
2017-08-20 08:45:45 +0800

    修改为通用版

--- webbase/db/oracle/init01.sql
+++ webbase/db/oracle/init01.sql
@@ -1,8 +1,7 @@
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('0','数据字典','',0,NULL,'s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('user.type','用户类别','user.type',1,'0','s',1);
-INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('2','员工','A',2,'user.type','s',1);
-INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('3','学生','S',3,'user.type','s',1);
-INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('4','合作用户','C',4,'user.type','s',1);
+INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('2','管理领导','L',2,'user.type','s',1);
+INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('3','普通用户','N',3,'user.type','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('user.status','用户状态','user.status',5,'0','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('6','正常','N',6,'user.status','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('7','锁定','L',7,'user.status','s',1);
@@ -20,8 +19,6 @@ INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('26','女','w'
 
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('org.type','机构类别','org.type',5,'0','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('ot1','公司','1',1,'org.type','s',1);
-INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('ot2','分公司','2',2,'org.type','s',1);
-INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('ot3','学校','3',3,'org.type','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('ot4','部门','4',4,'org.type','s',1);
 
 
@@ -65,7 +62,7 @@ VALUES('sys-user-add','新增','/user/add','sys-user','0',1,9103);
 INSERT INTO t_sys_privilege(pr_id,pr_name,Path,p_pr_id,pr_type,valid,Order_No)
 VALUES('sys-user-update','修改','/user/update','sys-user','0',1,9104);
 INSERT INTO t_sys_privilege(pr_id,pr_name,Path,p_pr_id,pr_type,valid,Order_No)
-VALUES('sys-user-remove','新增','/user/remove','sys-user','0',1,9105);
+VALUES('sys-user-remove','删除','/user/remove','sys-user','0',1,9105);
 INSERT INTO t_sys_role_priv(r_id,pr_id) VALUES('admin','sys-user');
 INSERT INTO t_sys_role_priv(r_id,pr_id) VALUES('admin','sys-user-query');
 INSERT INTO t_sys_role_priv(r_id,pr_id) VALUES('admin','sys-user-add');
@@ -82,7 +79,7 @@ VALUES('sys-role-add','新增','/role/add','sys-role','0',1,9113);
 INSERT INTO t_sys_privilege(pr_id,pr_name,Path,p_pr_id,pr_type,valid,Order_No)
 VALUES('sys-role-update','修改','/role/update','sys-role','0',1,9114);
 INSERT INTO t_sys_privilege(pr_id,pr_name,Path,p_pr_id,pr_type,valid,Order_No)
-VALUES('sys-role-delete','新增','/role/delete','sys-role','0',1,9115);
+VALUES('sys-role-delete','删除','/role/delete','sys-role','0',1,9115);
 INSERT INTO t_sys_privilege(pr_id,pr_name,Path,p_pr_id,pr_type,valid,Order_No)
 VALUES('sys-role-get','获取','/role/get','sys-role','0',1,9116);
 INSERT INTO t_sys_role_priv(r_id,pr_id) VALUES('admin','sys-role');
