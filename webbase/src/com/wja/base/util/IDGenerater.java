686658baa163a96de5f4ea88ad19fe3e9f824d9a
wjacl <wang_jian_an@139.com>
2017-11-05 22:34:24 +0800

    加入新的工具：短信、id生成服务，汉字拼音首字母获取

---
+++ webbase/src/com/wja/base/util/IDGenerater.java
@@ -0,0 +1,42 @@
+package com.wja.base.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class IDGenerater
+{
+    private static SimpleDateFormat yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
+    
+    private static Calendar lastCa = Calendar.getInstance();
+    
+    private static String lastTimeStr = yyyyMMddHHmmss.format(lastCa.getTime());
+    
+    private static int seqNo = 0;
+    
+    /**
+     * 
+     * 获取一个yyyyMMddHHmmss+6位序号的ID
+     * 
+     * @return
+     * @see [类、类#方法、类#成员]
+     */
+    public static synchronized String getAnYMDHMSid(String prefix)
+    {
+        Calendar ca = Calendar.getInstance();
+        if (ca.equals(lastCa))
+        {
+            seqNo++;
+        }
+        else
+        {
+            lastCa = ca;
+            lastTimeStr = yyyyMMddHHmmss.format(lastCa.getTime());
+            seqNo = 1;
+        }
+        
+        return (StringUtils.isBlank(prefix) ? "" : prefix.trim()) + lastTimeStr
+            + StringUtils.leftPad("" + seqNo, 6, '0');
+    }
+}
